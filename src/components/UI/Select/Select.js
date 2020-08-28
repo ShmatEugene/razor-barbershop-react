7c9bcfc4026fd63fc8e34647165941e0f4d325e9
ShmatEugene <shmat.evg@yandex.ru>
2020-08-28 20:29:36 +0300

    Select component

---
+++ src/components/UI/Select/Select.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './Select.scss';
+
+export default function Select() {
+  const selectOptions = [
+    'Haircut & Style',
+    'Creative cut',
+    'Beard sculpt',
+    'Wash & Restyle',
+    'Beard trim',
+    'Children`s haircut',
+    'Scissor Cut',
+  ];
+
+  const [isPopUpOpen, setPopUpState] = React.useState(false);
+  const [activeItem, setActiveItem] = React.useState(selectOptions[0]);
+  const selectLabelRef = React.useRef(null);
+  const selectArrowRef = React.useRef(null);
+  const selectPopUpRef = React.useRef(null);
+
+  const bodyClickHandler = (e) => {
+    if (
+      !e.path.includes(selectLabelRef.current) &&
+      !e.path.includes(selectArrowRef.current) &&
+      !e.path.includes(selectPopUpRef.current)
+    ) {
+      setPopUpState(false);
+    }
+  };
+
+  const togglePopUp = () => {
+    setPopUpState(!isPopUpOpen);
+  };
+
+  const onSelectItem = (option) => {
+    setActiveItem(option);
+    setPopUpState(false);
+  };
+
+  React.useEffect(() => {
+    document.body.addEventListener('click', bodyClickHandler);
+  }, []);
+
+  return (
+    <>
+      <div ref={selectLabelRef} className="ui-select-label" onClick={togglePopUp}>
+        <span>{activeItem}</span>
+      </div>
+      <div ref={selectArrowRef} className="ui-select-arrow sh-grey-font aling-self-center sh-pl-10">
+        <i className="fa fa-chevron-down"></i>
+      </div>
+      {isPopUpOpen && (
+        <div ref={selectPopUpRef} className="ui-select-popup">
+          <ul>
+            {selectOptions.map((option, index) => {
+              return (
+                <li
+                  className={activeItem === option ? 'active' : ''}
+                  key={option + index}
+                  onClick={() => onSelectItem(option)}>
+                  {option}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.css';
 import 'slick-carousel/slick/slick.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
 
 import App from './App';
